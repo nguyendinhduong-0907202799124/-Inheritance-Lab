4fe036ee7fe13816d32545a611138db721819f4b
nguyendinhduong-0907202799124 <nguyendinhduongruoi2005@gmail.com>
2024-09-18 11:07:05 +0700

    Add files via upload

---
+++ Randomlist/list.py
@@ -0,0 +1,7 @@
+import random
+
+class RandomList:
+    def get_random_element(self):
+        random_list = random.randint(1, len(self) - 1)
+    
+    
---
+++ Single/dog.py
@@ -0,0 +1,4 @@
+from animal import Animal
+class Dog(Animal):
+    def dark(self):
+        return "barking..."
